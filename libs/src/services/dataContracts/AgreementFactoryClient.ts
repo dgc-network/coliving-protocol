f05af319edd5f2a481cb36e1a51485e786d6ef56
Rover Chen <rover.k.chen@gmail.com>
2022-10-08 07:27:21 +0800

    signatureSchemas

--- libs/src/services/dataContracts/AgreementFactoryClient.ts
+++ libs/src/services/dataContracts/AgreementFactoryClient.ts
@@ -1,6 +1,6 @@
 import { ContractClient } from '../contracts/contractClient'
 //import * as signatureSchemas from '../../../data-contracts/signatureSchemas'
-import * as signatureSchemas from '../../../../data-contracts/signatureSchemas'
+import * as signatureSchemas from '../../../../data-contracts/signatureSchemas/signatureSchemas'
 import type { Web3Manager } from '../web3Manager'
 
 export class AgreementFactoryClient extends ContractClient {
